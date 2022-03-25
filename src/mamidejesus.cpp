2beee683034d397e22b3d135f81d17b185658fd2
Alvaro1107 <aladron1107@gmail.com>
2022-03-25 16:17:14 +0100

    qañldnhiosl<zdas

--- src/mamidejesus.cpp
+++ src/mamidejesus.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "la madre de jesus la chupa" << endl; // prints !!!Hello World!!!
 	return 0;
 }
